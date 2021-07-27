2034b8f3aacd3af63058fbffc523a7a15d2ed476
Fonsciacob <70414397+Fonsciacob@users.noreply.github.com>
2021-07-27 09:59:44 -0500

    third commit

---
+++ Proyecto Final-3ra Entrega/animation/signUp.js
@@ -0,0 +1,8 @@
+$("#btnSignUp").click(() => {
+  $("#loginUser").css("width", "0");
+  $("#loginUser").css("overflow", "hidden");
+  $("#loginUser").css("opacity", "0");
+  $("#loginUser").css("transition", "all .3s");
+
+  $("#signUp").css("width", "490px");
+});
